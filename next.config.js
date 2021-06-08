eeb892ae7c404e6587a585061e49a8a150ae1d3a
nicolas hedoire <nicolashedoire@MacBook-Pro-de-nicolas.local>
2021-06-08 07:16:51 +0200

    add next config to export

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+    ) {
+      return {
+        '/': { page: '/' }
+      }
+    },
+    images: {
+      loader: "imgix",
+      path: "",
+    },
+  }
